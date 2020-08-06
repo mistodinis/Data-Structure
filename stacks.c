209e318309babd8d4a8c5acc5f61ae2a8aaf7750
mistodinis <68851085+mistodinis@users.noreply.github.com>
2020-08-06 16:07:59 +0300

    Update and rename main.c to stacks.c

--- stacks.c
+++ stacks.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+//Struct of a node
 typedef struct node{
 
     int value;
     struct node* next;
 }Node;
-
+// Struct of a stack
 typedef struct stack{
     Node *top;
     int size;
 
 }Stack;
-
+// Init a node
 Node* initNode(int value){
 
     Node* newNode = (struct node*)malloc(sizeof(struct node));
@@ -22,7 +22,7 @@ Node* initNode(int value){
 
     return newNode;
 }
-
+// Init a stack
 Stack *initStack(int value){
 
     Stack *newStack = (struct stack*)malloc (sizeof(struct stack));
@@ -31,11 +31,11 @@ Stack *initStack(int value){
 
     return newStack;
 }
-
+// Return value from the top
 int top(Stack *s){
     return s->top->value;
 }
-
+// Push an element to the top
 Stack *push(Stack *s, int value){
 
     if(s == NULL)
@@ -49,7 +49,7 @@ Stack *push(Stack *s, int value){
 
     return s;
 }
-
+// Delete an element from the top
 Stack *pop(Stack *s){
 
     if(s == NULL)
@@ -66,7 +66,7 @@ Stack *pop(Stack *s){
     return s;
 
 }
-
+// Return size of stack
 int size(Stack *s){
 
     return s->size;
@@ -100,30 +100,36 @@ int emptyStack(Stack *s){
         return 1;
     return 0;
 }
-
+// Function that has the main objective to see if an string has a good parentheses equitation
+//For example : (()) - is a good parentheses equitation , because there are 2 opened and 2 closed
+// (()() - is not a good equitation  , there are 3 opened and only 2 closed
 int parantezare(Stack *s , char *str){
-
+    // We parse the string
    for(int i = 0;i < strlen(str); i++){
-
+        // if we find an opened parenthesis we push "1" into the stack
         if(str[i] == '('){
-            //printf("DA // ");
+            //if the stack is empty we initialize stack
             if(s == NULL){
                 s = initStack(1);
             }else{
                 s = push(s,1);
             }
         }
-
+        // if we find an closed parenthesis , we have to pop an element from the stack
         if(str[i] == ')'){
-              //  printf("NU // ");
+              // if there are no element into the stack that means , there are too many ")"
             if(s == NULL){
+                // return 0 , means is not a good equitation
                 return 0;
-                exit(0);
+                //exit(0);
             }else{
                 s = pop(s);
             }
         }
    }
+    //After we end the string  , we check if the number of "(" are equal to number of ")"
+    // We check that by looking at the stack , if the stack is empty that means all the items pushed were finally poped 
+    // and result a good equitation
    if(emptyStack(s) == 1)
     return 1;
    else
