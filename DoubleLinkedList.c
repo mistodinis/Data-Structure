b817ff4637c6b6980dfeef0c3656b98344937d5e
mistodinis <68851085+mistodinis@users.noreply.github.com>
2020-08-06 18:39:20 +0300

    Add files via upload

---
+++ DoubleLinkedList.c
@@ -0,0 +1,348 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+	struct node *prev;
+}Node;
+
+typedef struct TDoubleLinkedList{
+	Node *head;
+	Node *tail;
+	int len;
+}TDoubleLinkedList;
+
+
+Node * initNode(int value){
+
+	Node *new_node = (struct node*) malloc(sizeof(struct node));
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->data = value;
+
+	return new_node;
+}
+
+TDoubleLinkedList* initList(int value){
+
+	TDoubleLinkedList *new_list = (struct TDoubleLinkedList*) malloc (sizeof(struct TDoubleLinkedList));
+	
+	Node * new_node = initNode(value);
+
+	new_list->head = new_node;
+	new_list->tail = new_node;
+	new_list->len = 1;
+
+	return new_list;
+}
+
+TDoubleLinkedList* addLast(TDoubleLinkedList *list , int value){
+
+	if(list == NULL || list->tail == NULL)
+		return initList(value);
+
+	Node *new_node = initNode(value);
+	list->tail->next = new_node;
+	new_node->prev = list->tail;
+	list->tail = list->tail->next;
+	list->len++;
+	return list;
+}
+
+TDoubleLinkedList *addFirst(TDoubleLinkedList *list , int value){
+
+	if(list == NULL || list->head == NULL){
+		return initList(value);
+	}
+
+	Node* new_node = initNode(value);
+	new_node->next = list->head;
+	list->head->prev = new_node;
+	list->head = list->head->prev;
+	list->len++;
+
+	return list;	
+}
+
+TDoubleLinkedList* addAtPosition(TDoubleLinkedList *list , int position , int value){
+
+	if(list == NULL || list->head == NULL){
+		list = initList(value);
+		//printf("The list was empty , the value was inserted at first position \n");
+		return list;
+	}
+
+	TDoubleLinkedList *temp = list;
+	Node *new_node = initNode(value);
+	Node *first = list->head;
+	Node *last = list->tail;
+
+	if(position == 1){
+		return addFirst(list,value);
+	}
+
+	if(position > list->len){
+		return addLast(list,value);
+	}
+	
+	if(position <= (list->len)/2){
+		
+		int counter = 1;
+		while(temp->head != NULL && counter != position-1){
+			
+			if(temp->head->next != NULL){
+				temp->head = temp->head->next;
+			}else
+				break;
+			counter++;
+		}
+		
+		new_node->next = temp->head->next;
+		temp->head->next = new_node;
+		new_node->prev = temp->head;
+		
+		if(new_node->next != NULL){
+			new_node->next->prev = new_node;
+		}
+		
+		temp->head = first;
+		list->len++;
+
+		return list;
+	}else{
+
+		int moves = list->len - position + 1;
+		while(temp->tail != NULL && moves != 0){
+			temp->tail = temp->tail->prev;
+			moves--;
+		}
+		
+		new_node->next = temp->tail->next;
+		temp->tail->next = new_node;
+		new_node->prev = temp->tail;
+		temp->tail = last;
+		
+		if(new_node->next != NULL)
+			new_node->next->prev = new_node;
+		
+		list->len++;
+
+		return list;
+	}
+}
+
+
+TDoubleLinkedList *deleteElem(TDoubleLinkedList *list , int value){
+
+	if(list == NULL || list->head == NULL)
+		return list;
+
+	if(list->head->data == value){
+		
+		if(list->head->next != NULL){
+		
+			Node *tmp = list->head;
+			list->head = list->head->next;
+			free(tmp);
+			list->len--;
+			return list;
+		}else{
+			return NULL;
+		}
+	}
+
+	TDoubleLinkedList *tmp = list;
+	Node *prev = list->head;
+	Node *first = list->head;
+	tmp->head = tmp->head->next;
+	
+	while(tmp->head != NULL && tmp->head->data != value){
+		
+		if(tmp->head->next == NULL)
+			break;
+		
+		tmp->head = tmp->head->next;
+		prev = prev->next;
+	}
+
+	if(tmp->head->data == value){
+		
+		if(tmp->head->next == NULL){
+		
+			prev->next = NULL;
+			free(tmp->head);
+			list->len--;
+			tmp->head = first;
+			return list;
+		}else{
+			
+			prev->next = tmp->head->next;
+			prev->next->prev = prev;
+			free(tmp->head);
+			tmp->head = first;
+			list->len--;
+			tmp->head = first;
+			return list;
+		}
+	}
+	list->head = first;
+	return list;
+}
+
+TDoubleLinkedList *deleteList(TDoubleLinkedList *list){
+
+	if(list == NULL)
+		return list;
+
+	list = deleteElem(list,list->head->data);
+	return deleteList(list);
+}
+
+
+void printList(TDoubleLinkedList *list){
+
+	if( list == NULL || list->head == NULL){
+		printf("The list is empty \n");
+		return;
+	}
+
+	if(list->head->next == NULL){
+		printf("%d \n" , list->head->data);
+		return;
+	}
+	
+	TDoubleLinkedList *temp = list;
+	Node *tmp = list->head;
+	
+	while(temp->head->next != NULL){
+
+		printf("%d ", temp->head->data );
+		temp->head = temp->head->next;
+	}
+
+	printf("%d " , temp->head->data);
+	temp->head = tmp;
+
+	printf("\n");
+}
+
+void printReversed(TDoubleLinkedList *list){
+
+	if(list == NULL || list->head == NULL || list->tail == NULL){
+		printf("The list is empty\n");
+		return;
+	}
+
+	if(list->tail->prev == NULL){
+		printf("%d \n" , list->tail->data );
+		return;
+	}
+
+	TDoubleLinkedList *temp = list;
+	Node *last = list->tail;
+
+	while(temp->tail != NULL){
+
+		printf("%d ", temp->tail->data);
+
+		if(temp->tail->prev == NULL)
+			break;
+
+		temp->tail = temp->tail->prev;
+	}
+
+	temp->tail = last;
+	printf("\n");
+}
+
+
+TDoubleLinkedList *add_in_order(TDoubleLinkedList *list , int value){
+
+
+	if(list == NULL){
+		return initList(value);
+	}
+
+	Node *new_node = initNode(value);
+	Node *first = list->head;
+	TDoubleLinkedList *temp = list;
+	
+	if(list->head != NULL){
+		if(list->head->data > value)
+			return addFirst(list,value);
+	}
+
+	while(temp->head->next != NULL ){
+
+		if(value > temp->head->data && value <= temp->head->next->data){
+		
+			new_node->next = temp->head->next;
+			new_node->prev = temp->head;
+			temp->head->next = new_node;
+			new_node->next->prev = new_node;
+			
+			temp->head = first;
+			list->len++;
+			
+			return list;
+		}
+
+		if(temp->head->next->next == NULL)
+			break;
+		temp->head = temp->head->next;
+	}
+	temp->head = first;
+	return addLast(list,value);
+}
+
+
+int main(){
+
+	TDoubleLinkedList *list = initList(10);
+	printList(list);
+
+	list = addFirst(list , 3);
+	list = addFirst(list , 4);
+	list = addFirst(list , 7);
+	list = addFirst(list , 9);
+
+	list = addLast(list,7);
+	list = addLast(list,69);
+	list = addLast(list,35);
+	
+	printList(list);
+	printf("len = %d \n" , list->len);
+
+	int position; printf("position = " ); scanf("%d" , &position); printf("\n");
+
+	list = addAtPosition(list , position , -222);
+	printList(list);
+
+	int del; printf("DelElem = "); scanf("%d" , &del); printf("\n");
+	list = deleteElem(list, del);
+	printList(list); printf("\n");
+	
+	//printReversed(list);
+
+	//list = deleteList(list);
+	//printList(list);
+
+	TDoubleLinkedList *ord_list = initList(1);
+	ord_list = addLast(ord_list , 2);
+	ord_list = addLast(ord_list , 3);
+	ord_list = addLast(ord_list , 5);
+	ord_list = addLast(ord_list , 7);
+	ord_list = addLast(ord_list , 9);
+	ord_list = addLast(ord_list , 12);
+
+	printList(ord_list);
+
+	int ordItem; printf("Elem to ord list = "); scanf("%d" , &ordItem);
+	ord_list = add_in_order(ord_list, ordItem);
+
+	printList(ord_list);
+
+	return 0;
+
+}
